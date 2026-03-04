e64b4796ad16a3c6d6d7e7391c52aff9d5b3214d
Andrii <pxlgrow.com@gmail.com>
2026-03-04 23:16:35 +0200

    speed and accessibility optimization

--- app/layout.js
+++ app/layout.js
@@ -51,7 +51,7 @@ export default function RootLayout({ children }) {
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800;900&amp;family=Syne:wght@400;500;600;700;800&amp;display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@400;500;600;700&amp;family=Syne:wght@700;800&amp;display=swap" rel="stylesheet" />
       </head>
       <body>{children}</body>
     </html>
