0f0cafca21de5dd01954abf7b73ca484220a3760
Andrii <pxlgrow.com@gmail.com>
2026-03-06 23:24:29 +0200

    add SEO routes - 15 pages

--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
 "use client"
 import dynamic from "next/dynamic"
 
-const PxlGrowSite = dynamic(() => import("PxlGrowSite"), { ssr: false })
+const PxlGrowSite = dynamic(() => import("./PxlGrowSite"), { ssr: false })
 
 export default function Page() {
   return <PxlGrowSite initialPage="home" />
